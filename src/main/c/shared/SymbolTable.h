beb7b3545c34161bfb162890b6bdcc65bfdbc212
fbon304 <franbon2003@gmail.com>
2025-06-21 17:37:03 -0300

    SymbolTable.c finished, continuing with semantic validations

--- src/main/c/shared/SymbolTable.h
+++ src/main/c/shared/SymbolTable.h
@@ -21,18 +21,25 @@ typedef struct Symbol {
     DataType type;
 	DataValue value;
     boolean isPrimaryKey;
+    boolean isUnique;
 } Symbol;
 
 void initializeSymbolTableModule(CompilerState * compilerState);
 void shutdownSymbolTableModule();
 void addTableToSymbolTable(HashMap * symbolTable, const char *tableName);
-int putVariableInScope(HashMap * symbolTable, const char * scope, const char * variableName, DataValue dataValue, DataType dataType);
+int putVariableInScope(HashMap * symbolTable, const char * scope, const char * variableName, DataType dataType);
+int putValueInVariableInScope(HashMap * symbolTable, const char * scope, const char * variableName, DataValue dataValue);
+void setIsPrimaryKey(HashMap * symbolTable, const char * scope, const char * variableName);
+void setIsUnique(HashMap * symbolTable, const char * scope, const char * variableName);
 void destroySymbolTable(HashMap * symbolTable);
 int variableExistsInScope(HashMap * symbolTable, const char * scope, const char * variableName);
 boolean tableExistsInSymbolTable(HashMap * symbolTable, const char * tableName);
 DataType type(HashMap * symbolTable, const char * scope, const char * variableName);
 DataValue * getValue(HashMap * symbolTable, const char * scope, const char * variableName);
+boolean getIsPrimaryKey(HashMap * symbolTable, const char * scope, const char * variableName);
+boolean getIsUnique(HashMap * symbolTable, const char * scope, const char * variableName);
 Symbol * getEntry(HashMap * symbolTable, const char * scope, const char * variableName);
-boolean pushScope(Stack *scopeStack, const char *scopeName);
-char *popScope(Stack *scopeStack);
+boolean pushScope(Stack *scopeStack, char *scopeName);
+char * popScope(Stack *scopeStack);
+char * getScope(Stack * scopeStack);
 #endif
